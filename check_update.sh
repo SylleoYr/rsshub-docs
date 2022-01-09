32984deb7255d61920fa8b28fc5d6d37f2d33cd1
SylleoYr <SylleoYr@users.noreply.github.com>
2022-01-09 10:37:18 +0800

    correct check_update script

--- check_update.sh
+++ check_update.sh
@@ -4,7 +4,7 @@
  # @Author: Sylleo
  # @Version: 0.0.1
  # @Date: 2022-01-09 10:00:08
- # @LastEditTime: 2022-01-09 10:12:13
+ # @LastEditTime: 2022-01-09 10:36:13
  # @LastEditors: Sylleo
  # @Description: 
  # @FilePath: /rsshub-docs/check_update.sh
@@ -12,10 +12,13 @@
  # Copyright (C) 2022 Sylleo. All rights reserved.
 ### 
 
-if [-n "$(git status -s)"]; then
+if [ -n "$(git status -s)" ]; then
+    git status
     git config user.name SylleoYr
     git config user.email SylleoYr@users.noreply.github.com
     git add .
     git commit -m "Docs updated by GitHub Actions"
     git push -u origin master
+else
+    echo "There is no update."
 fi
